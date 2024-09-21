7453febee6a72e3b41b0167d44541c3c71602041
Swagat1126 <150437976+Swagat1126@users.noreply.github.com>
2024-09-22 00:21:45 +0530

    Update TODO.jsx

--- TODO.jsx
+++ TODO.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TODO = () => {
   return ( 
   <div className='text-center  text-3xl underline font-bold '>
-    <h1>TODO</h1>
+    <h1>TODO List</h1>
     </div>
   )
    
