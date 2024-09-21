dc3a319d259168a2143a9f3eb951f775d6baccce
Swagat1126 <150437976+Swagat1126@users.noreply.github.com>
2024-09-22 00:20:00 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,6 @@
 import { useState } from 'react'
-//  import reactLogo from './assets/react.svg'
-//  import viteLogo from '/vite.svg'
-// import './App.css'
-//  import Signup from './component/Signup'
-//  import MentorContactInfoForm from './component/MentorContactInfoForm'
-//  import CandidateContactInfoForm from './component/StudentContactInfoForm'
-// import Test from './component/Test'
 import TODO from './component/TODO'
 import TodoList from './component/TodoList'
-import MyComponent from './component/MyComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +9,6 @@ function App() {
     <>
      <TODO/>
      <TodoList/>
-     <MyComponent/>
   
     </>
   )
